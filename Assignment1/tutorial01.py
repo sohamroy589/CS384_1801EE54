977f64991ba84c271dffa15b3b8e1e79e89df8e5
Soham Roy <roysoham58@gmail.com>
2020-09-16 19:33:35 +0530

    Implemented my first basic python calculator.

---
+++ Assignment1/tutorial01.py
@@ -0,0 +1,23 @@
+# Function to add two numbers 
+def add(num1, num2): 
+	addition = num1 + num2
+	return addition
+
+# Function to subtract two numbers 
+def subtract(num1, num2): 
+	subtraction = num1 - num2
+	return subtraction
+
+# Function to multiply two numbers 
+def multiply(num1, num2): 
+	multiplication = num1 * num2
+	return multiplication
+
+# Function to divide two numbers 
+def divide(num1, num2): 
+	if num2 == 0:
+		print("Can't divide by zero!")
+		return 0
+	division = num1 / num2
+	return division
+	
