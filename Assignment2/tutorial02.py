ad76e75c1706d43c495646c62a41bbc17e2dfc89
Soham Roy <roysoham58@gmail.com>
2020-09-23 01:10:40 +0530

    Implemented the not so fast sorting function using bubble sort algorithm

--- Assignment2/tutorial02.py
+++ Assignment2/tutorial02.py
@@ -80,8 +80,13 @@ def skewness(first_list):
     return skewness_value
     
 def sorting(first_list):
-    sorted_list = []
-    # Sorting Logic
+    sorted_list = first_list
+    for r in range(len(first_list)-1, 0, -1):
+        for i in range(r):
+            if sorted_list[i] > sorted_list[i+1]:
+                temp = sorted_list[i]
+                sorted_list[i] = sorted_list[i+1]
+                sorted_list[i+1] = temp
     return sorted_list
 
 
