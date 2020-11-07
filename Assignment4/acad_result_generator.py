41241eb0fd9a9fef3fa6748fbe09afe2ff1ad617
Soham Roy <roysoham58@gmail.com>
2020-11-07 17:10:10 +0530

    Assignment-4 : Added acad_result_generator.py file

---
+++ Assignment4/acad_result_generator.py
@@ -0,0 +1,11 @@
+import os
+import csv
+
+info = {}
+
+def result_generator():
+    os.chdir(r'C:\Users\hp\Desktop\CS384_1801EE54\Assignment4')
+    print(os.getcwd())
+
+if __name__ == '__main__':
+    result_generator()
