8c98487f2c47a853500995538e3da99caa1973c1
Soham Roy <roysoham58@gmail.com>
2020-11-13 19:53:38 +0530

    Assignment 5: Implemented the rename_Sherlock function

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -26,8 +26,8 @@ def rename_Game_of_Thrones(folder_name):
     for file in os.listdir(folder_name):
         season, ep_no, ep_name = re.findall(r'(\d+)(?:\D+)(\d+)\W*([^\.]+)', file)[0]
         extension = re.search(r'\.mp4|\.srt', file).group()
-        season.lstrip('0')
-        ep_no.lstrip('0')
+        season = season.lstrip('0')
+        ep_no = ep_no.lstrip('0')
         new_filename = folder_name + ' - Season ' + '0'*(season_padding - len(season)) + season + ' Episode ' + '0'*(episode_padding - len(ep_no)) + ep_no + ' - ' + ep_name + extension
         try:
             os.rename(os.path.join(folder_name, file), os.path.join(folder_name, new_filename))
@@ -36,8 +36,19 @@ def rename_Game_of_Thrones(folder_name):
     
 
 def rename_Sherlock(folder_name):
-    # rename Logic 
-    pass
+    season_padding = int(input("Enter the season number padding...\n"))
+    episode_padding = int(input('Enter the episode number padding...\n'))
+    os.chdir('Subtitles')
+    for file in os.listdir(folder_name):
+        season, ep_no = re.findall(r'(?:S\w*)\s*(\d+)\W*(?:E\w*)\s*(\d+)', file)[0]
+        extension = re.search(r'\.mp4|\.srt', file).group()
+        season = season.lstrip('0')
+        ep_no = ep_no.lstrip('0')
+        new_filename = folder_name + ' - Season ' + '0'*(season_padding - len(season)) + season + ' Episode ' + '0'*(episode_padding - len(ep_no)) + ep_no + extension
+        try:
+            os.rename(os.path.join(folder_name, file), os.path.join(folder_name, new_filename))
+        except FileExistsError:
+            os.remove(os.path.join(folder_name, file))
     
 
 def rename_Suits(folder_name):
@@ -56,3 +67,5 @@ if __name__ == '__main__':
         rename_FIR('FIR')
     elif folder == 'game of thrones':
         rename_Game_of_Thrones('Game of Thrones')
+    elif folder == 'sherlock':
+        rename_Sherlock('Sherlock')
