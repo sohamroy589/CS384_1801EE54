8d5a47fedce4bbdf6245db47934c8e997ffdded3
Soham Roy <roysoham58@gmail.com>
2020-11-13 20:00:50 +0530

    Assignment 5: Implemented the rename_Suits function

--- Assignment5/tutorial05.py
+++ Assignment5/tutorial05.py
@@ -52,8 +52,19 @@ def rename_Sherlock(folder_name):
     
 
 def rename_Suits(folder_name):
-    # rename Logic 
-    pass
+    season_padding = int(input("Enter the season number padding...\n"))
+    episode_padding = int(input('Enter the episode number padding...\n'))
+    os.chdir('Subtitles')
+    for file in os.listdir(folder_name):
+        season, ep_no, ep_name = re.findall(r'(\d+)(?:\D+)(\d+)\W*([^\.]+)', file)[0]
+        extension = re.search(r'\.mp4|\.srt', file).group()
+        season = season.lstrip('0')
+        ep_no = ep_no.lstrip('0')
+        new_filename = folder_name + ' - Season ' + '0'*(season_padding - len(season)) + season + ' Episode ' + '0'*(episode_padding - len(ep_no)) + ep_no + ' - ' + ep_name + extension
+        try:
+            os.rename(os.path.join(folder_name, file), os.path.join(folder_name, new_filename))
+        except FileExistsError:
+            os.remove(os.path.join(folder_name, file))
     
 
 def rename_How_I_Met_Your_Mother(folder_name):
@@ -69,3 +80,5 @@ if __name__ == '__main__':
         rename_Game_of_Thrones('Game of Thrones')
     elif folder == 'sherlock':
         rename_Sherlock('Sherlock')
+    elif folder == 'suits':
+        rename_Suits('Suits')
